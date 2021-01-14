220dfe6e17698e600cc4381375a9b604a8160bce
eli <elilevin11@gmail.com>
2021-01-14 19:17:32 +0200

    Started doing reactions

--- modules/poll/cog.py
+++ modules/poll/cog.py
@@ -12,14 +12,22 @@ class PollCog(commands.Cog):
 	@commands.command(name="poll")
 	async def poll(self, ctx, *args):
 		""" A command to create new user polls """
-
 		# Create the Poll object
 		poll = Poll(ctx.message)
-		msg = await ctx.send(embed=poll.embed, delete_after=poll.duration)
+		self.msg = await ctx.send(embed=poll.embed, delete_after=poll.duration)
 		for i in range(len(poll.options)):
-			await msg.add_reaction(poll.emojis[i])
+			await self.msg.add_reaction(poll.emojis[i])
 		# delete the og message
 		await ctx.message.delete()
+
+	@commands.Cog.listener
+	async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
+		if self.msg.id == reaction.message.id:
+			voted = await self.msg.users().flatten()
+			emoji = reaction.emoji
+			id = user.id
+			if user in voted:
+				await self.msg.remove_reaction(emoji)
 
 
 def setup(bot):
